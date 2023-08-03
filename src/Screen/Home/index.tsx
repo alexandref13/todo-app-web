de36a87f22d868be3b5f524866e0f6819960bcef
Alexandre Fernandes <alexfernandes199922@gmail.com>
2023-08-03 13:29:07 -0300

    feat: content

--- src/Screen/Home/index.tsx
+++ src/Screen/Home/index.tsx
@@ -1,6 +1,62 @@
-import { Button, Container, Form, Header, Input, Main, Text } from "./styled";
+import { useState, useEffect } from "react";
+
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { FiTrash } from "react-icons/fi";
+
+import { 
+  Button,
+  Container, 
+  Content, 
+  FinishedTasksText, 
+  Form, 
+  Header, 
+  HeaderContent, 
+  Input, 
+  Main, 
+  TaskContainer, 
+  TaskTitle, 
+  TasksCreatedText, 
+  TasksHeaderContainer, 
+  TasksValueContainer, 
+  TasksValueText, 
+  Text 
+} from "./styled";
+
+interface TasksProps {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+}
+
+const DATA_TASK = [
+  {
+    id: 1,
+    title: "Ir à academia",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    title: "Fazer as compras",
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    title: "Ir à formatura do Moisés",
+    isCompleted: true,
+  },
+]
 
 export function Home(){
+  const [ tasks, setTasks ] = useState<TasksProps[]>([])
+
+  function fetchTasks(){
+    setTasks(DATA_TASK)
+  }
+
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
   return (
     <Container>
       <Header/>
@@ -11,6 +67,33 @@ export function Home(){
             <Text> Criar </Text>
           </Button>
         </Form>
+
+        <Content>
+          <HeaderContent>
+            <TasksHeaderContainer>
+              <TasksCreatedText>Tarefas criadas</TasksCreatedText>
+              <TasksValueContainer>
+                <TasksValueText>5</TasksValueText>
+              </TasksValueContainer>
+            </TasksHeaderContainer>
+
+            <TasksHeaderContainer>
+              <FinishedTasksText>Concluídas</FinishedTasksText>
+              <TasksValueContainer>
+                <TasksValueText>2 de 5</TasksValueText>
+              </TasksValueContainer>  
+            </TasksHeaderContainer>
+          </HeaderContent>
+
+          {tasks.map((task) => (
+            <TaskContainer key={task.id}>
+              <MdCheckBoxOutlineBlank size={24} color="#4EA8DE"/>
+              <TaskTitle>{task.title}</TaskTitle>
+              <FiTrash size={20}  color="#808080"/>
+            </TaskContainer>
+          ))}
+          
+        </Content>
       </Main>
 
     </Container>
--- src/Screen/Home/styled.ts
+++ src/Screen/Home/styled.ts
@@ -1,10 +1,11 @@
 import { styled } from "styled-components";
 
 export const Container = styled.div `
-
   width: 100vw;
   height: 100vh;
 `
+
+// HEADER
 
 export const Header = styled.header`
   width: 100%;
@@ -15,15 +16,16 @@ export const Header = styled.header`
 export const Main = styled.main`
  width: 100%;
  height: 80%;
- 
 
  background-color: #1A1A1A;
 
  display: flex;
-
- align-items: flex-start;
- justify-content: center;
+ flex-direction: column;
+ align-items: center;
+ justify-content: flex-start;
 `
+
+// FORM
 
 export const Form = styled.form`
   width: 50%;
@@ -47,10 +49,8 @@ export const Input = styled.input`
   font-size: 16px;
   font-weight: 400;
   color: #ffffff;
-
 `
 export const Button = styled.button`
-
   background-color: #1E6F9F;
   
   border: none;
@@ -66,6 +66,80 @@ export const Text = styled.p`
   font-size: 16px;
   font-weight: bold;
   color: #ffffff;
-
-
 `
+
+// CONTENT
+
+export const Content = styled.div`
+  width: 50%;
+  margin-top: 64px; 
+`
+
+export const HeaderContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+  justify-content: space-between;
+
+  margin-bottom: 25px;
+`
+
+export const TasksHeaderContainer = styled.div`
+  display: flex; 
+  flex-direction: row;
+  align-items: center;
+
+  gap: 8px;
+`
+
+export const TasksCreatedText = styled.p`
+  color:#4EA8DE;
+  font-size: 14px;
+  font-weight: 700;
+`
+
+export const FinishedTasksText = styled.p`
+  color:#8284FA;
+  font-size: 14px;
+  font-weight: 700;
+`
+
+export const TasksValueContainer = styled.div`
+  padding: 2px 8px;
+  background-color: #333;
+  border-radius: 999px;
+`
+
+export const TasksValueText = styled.p`
+  color: #D9D9D9;
+  font-size: 12px;
+  font-weight: 700;
+`
+
+export const TaskContainer = styled.div`
+  background-color: #262626;
+
+  padding: 16px; 
+
+  border-radius: 8px;
+  border: 1px solid #333;
+
+  box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.06);
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+`
+
+export const TaskTitle = styled.p`
+  display: flex;
+  flex: 2;
+
+  color: #F2F2F2;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 140%;
+`
+
